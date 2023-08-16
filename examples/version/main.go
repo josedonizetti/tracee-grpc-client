3c7b88ed7eaa7d787a312f66f364bcfb6166375b
Jose Donizetti <jdbjunior@gmail.com>
2023-08-16 08:44:35 -0300

    update examples

--- examples/version/main.go
+++ examples/version/main.go
@@ -5,16 +5,21 @@ import (
 	"log"
 
 	pb "github.com/aquasecurity/tracee/types/api/v1beta1"
-
-	"github.com/josedonizetti/tracee-grpc-client/pkg/client"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 
-	conn := client.NewGRPCConn()
-	defer conn.Close()
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
-	client := client.NewTraceeServciceClient(conn)
+	conn, err := grpc.Dial(":4466", opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+
+	client := pb.NewTraceeServiceClient(conn)
 
 	version, err := client.GetVersion(context.Background(), &pb.GetVersionRequest{})
 	if err != nil {
